4fef903d1eb5ed7a37a01bb4e43a73d279c134cf
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-03-09 14:29:01 +0100

    Started sol

--- src/main/java/io/github/oliviercailloux/persons_manager/MyPersonsManager.java
+++ src/main/java/io/github/oliviercailloux/persons_manager/MyPersonsManager.java
@@ -1,5 +1,11 @@
 package io.github.oliviercailloux.persons_manager;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * This class should be instanciated using one of its static factory method (to
@@ -12,8 +18,39 @@ package io.github.oliviercailloux.persons_manager;
  */
 class MyPersonsManager implements PersonsManager {
 
+	private Set<Person> persons;
+
 	MyPersonsManager() {
 		// …
 	}
 
+	@Override
+	public void setPersons(List<Person> persons) {
+		this.persons = new LinkedHashSet<>();
+		this.persons.addAll(persons);
+	}
+
+	@Override
+	public Map<Integer, Person> toMap() {
+		TODO();
+		return null;
+	}
+
+	@Override
+	public Iterator<Person> personIterator() {
+		TODO();
+		return null;
+	}
+
+	@Override
+	public Iterator<Integer> idIterator() {
+		TODO();
+		return null;
+	}
+
+	@Override
+	public RedundancyCounter getRedundancyCounter() {
+		TODO();
+		return null;
+	}
 }
