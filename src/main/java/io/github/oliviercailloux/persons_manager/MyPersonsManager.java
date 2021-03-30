8a54625e93303d30008e75b8102440d40eeb8ac5
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-03-30 10:57:56 +0200

    The provided constructor is package-private

--- src/main/java/io/github/oliviercailloux/persons_manager/MyPersonsManager.java
+++ src/main/java/io/github/oliviercailloux/persons_manager/MyPersonsManager.java
@@ -8,6 +8,10 @@ package io.github.oliviercailloux.persons_manager;
  * “given”, admits an iterable instance of persons as a parameter, and uses it
  * to initialize the set of persons that the returned instance will manage (this
  * also initializes correspondingly the redundancy counter).
+ * <p>
+ * The provided constructor is package-private (instead of private, usually
+ * recommended) to allow access from test methods. You can leave it as it is, it
+ * doesn’t hurt.
  *
  */
 class MyPersonsManager implements PersonsManager {
